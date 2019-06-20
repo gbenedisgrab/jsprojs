4d132cacdb7e71f39bd9eb62ac57529b283bd1c0
Gregory Benedis-Grab <gbenedisgrab@fsgreben1700548.packer.edu>
2019-06-19 23:00:44 -0400

    got cube working :

--- three-cube/sketch.js
+++ three-cube/sketch.js
@@ -1,59 +1,54 @@
-let scene,
-    renderer,
-    container,
-    width,
-    height;
+import {scene} as THREE from 'js/three.js';
 
-let camera,
-    fov, aspect, near, far;
+let scene, renderer, container, width, height;
+let cameara, fov, aspect, near, far;
+let cube=[];
 
 function init() {
   width = window.innerWidth;
   height = window.innerHeight;
-
   fov = 50;
-  aspect = width / height;
-  near = 1;
-  far = 2000;
-
+  aspect = width/height;
   scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.x = 300;
-  camera.position.z = 300;
+  camera = new THREE.PerspectiveCamera( fov, aspect, 1, 3000 );
+  camera.position.x = 200;
+  camera.position.z = 600;
   camera.position.y = 200;
-  camera.lookAt(new THREE.Vector3(0,0,0));
-
-  renderer = new THREE.WebGLRenderer({
-    antialias: true,
-  });
-  renderer.setSize(width, height);
+  camera.lookAt(new THREE.Vector3(320,0,0));
+  renderer = new THREE.WebGLRenderer({anitalias: true,});
+  renderer.setSize( width, height );
   renderer.shadowMap.enabled = true;
-
   container = document.getElementById('scene');
   container.appendChild(renderer.domElement);
 }
-
 function createBox() {
-  let geometry = new THREE.BoxGeometry( 100, 100, 100 );
-  let material = new THREE.MeshPhongMaterial( { color: 0xd97d34 } );
-  let cube = new THREE.Mesh( geometry, material );
-  cube.castShadow = true;
 
-  scene.add( cube );
+
+  for (let i=0;i<10;i++){
+    let geometry = new THREE.BoxGeometry( 30+3*i, 30+3*i, 30+3*i );
+    let color = 0xd97d34;
+    if (i%2 == 0){
+      color = 0x45324d;
+    }
+    let material = new THREE.MeshPhongMaterial( { color: color } );
+    let c = new THREE.Mesh( geometry, material );
+    c.castShadow=true;
+    c.position.set(i*80,0,0);
+    scene.add(c);
+    cube.push(c)
+  }
 }
 
 function createFloor() {
-  var planeGeometry = new THREE.PlaneBufferGeometry( 400, 400, 10, 10 );
+  var planeGeometry = new THREE.PlaneBufferGeometry( 1000, 400, 10, 10 );
   var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x45545a } )
   let plane = new THREE.Mesh( planeGeometry, planeMaterial );
-  plane.position.set(0, -100, 0);
+  plane.position.set(300, -100, 0);
 
   plane.receiveShadow = true;
   plane.rotation.x = -90 * Math.PI / 180;
   scene.add( plane );
 }
-
 function lighting() {
   const light = new THREE.DirectionalLight( 0xffffff, 1);
   light.position.set( -100, 500, 400 );
@@ -61,7 +56,7 @@ function lighting() {
 
   const d = 200;
   light.shadowCameraLeft = -d;
-  light.shadowCameraRight = d;
+  light.shadowCameraRight = d+600;
   light.shadowCameraTop = d;
   light.shadowCameraBottom = -d;
 
@@ -72,14 +67,18 @@ function lighting() {
   scene.add( new THREE.AmbientLight(0xa59f75, 0.6) );
 }
 
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-}
+var animate = function () {
+  requestAnimationFrame( animate );
+  for (let i=0;i<10;i++){
+    cube[i].rotation.x += .01;
+    cube[i].rotation.y += i*.01;
+    cube[i].rotation.z += (10-i)*(10-i) /1000;
+  }
 
+  renderer.render( scene, camera );
+};
 init();
-lighting();
 createBox();
 createFloor();
-
+lighting();
 animate();
